2afda4c659fdce4e65724ef3eddcace76c71c7e4
Giri-123s <163376224+Giri-123s@users.noreply.github.com>
2024-03-16 03:22:07 +0000

    variable

---
+++ .devcontainer/variable.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="shivam@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId=2
+accountEmail="q@gmail.com"
+accountPassword="54321"
+accountCity="bihar"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
